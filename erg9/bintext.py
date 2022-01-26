b64641d41f0f1f4f0d71b4c037e488474fc92f2e
Noveboi <cptgiorgos03@gmail.com>
2022-01-26 21:10:33 +0200

    erg9 complete

---
+++ erg9/bintext.py
@@ -0,0 +1,19 @@
+# Σας δίνεται αρχείο κειμένου με μόνο ASCII χαρακτήρες. Αρχικά απεικονίστε κάθε χαρακτήρα σε δυαδικό 
+# μήκους 7. Υπολογίστε ποια είναι η μεγαλύτερη ακολουθία από συνεχόμενα 0 και από συνεχόμενα 1.
+
+import os
+os.chdir('./erg9')
+
+with open('tale.txt', 'r') as f:
+    text = f.read()
+
+chars = list(text)
+binary = ''
+for char in chars:
+    binary += bin(ord(char))[2:]
+
+maxZeroCount = len(max(binary.split('1')))
+maxOneCount = len(max(binary.split('0')))
+print(maxZeroCount)
+print(maxOneCount)
+    
