c552e6e1ca1e98aa91b9fdd7b0d41146d7090af8
Noveboi <cptgiorgos03@gmail.com>
2022-01-26 17:05:07 +0200

    split .py files for ease of use, game mechanic complete

---
+++ erg2/entities.py
@@ -0,0 +1,76 @@
+class Cap():
+    def __init__(self, size):
+        self.size = size
+        self.pos = None
+
+    def sizeCompare(self, otherCap):
+        if self.size > otherCap: return "Bigger"
+        elif self.size < otherCap: return "Smaller"
+        return "Same"        
+    
+    def placeOnBoard(self, tile):
+        self.pos = tile
+        return self.size #so the value can be stored in the Board class as well
+
+class Board():
+#   1 2 3
+# a o o o
+# b o o o   <---- Board
+# c o o o
+
+    #a1,b2,c3 etc. represent tiles, they if a cap is placed on them their value becomes the size of the cap
+    def __init__(self):
+        self.tiles = {
+            'a1': None, 'a2': None, 'a3': None,
+            'b1': None, 'b2': None, 'b3': None,
+            'c1': None, 'c2': None, 'c3': None
+        }
+    
+    def check(self, mode): #rcd = row, column, diagonal
+        vert = ['a','b','c']
+        hor = ['1','2','3']
+        if mode == 'row':
+            for row in vert:
+                temp_row = []
+                for col in hor:
+                    key = f"{row}{col}"
+                    temp_row.append(self.tiles[key])
+                if temp_row == sorted(temp_row): 
+                    print("")
+                    return True
+            return False
+        elif mode == 'column':
+            for col in hor:
+                temp_col = []
+                for row in vert:
+                    key = f"{row}{col}"
+                    temp_col.append(self.tiles[key])
+                if temp_col == sorted(temp_col): 
+                    print("col win")
+                    return True
+            return False
+        elif mode == 'diagonal':
+            for i in range(2):
+                temp_diag = []
+                if i == 2: 
+                    hor.reverse()
+                for col, row in enumerate(vert):
+                    key = f"{row}{col+1}"
+                    temp_diag.append(self.tiles[key])
+                if temp_diag == sorted(temp_diag): 
+                    print("diag win")
+                    return True
+            return False
+
+    def finish(self):
+        if self.check('row') or self.check('column') or self.check('diagonal'): 
+            return True
+        return False
+
+    def show(self):
+        i = 0
+        for tile in self.tiles:
+            if i % 3 == 0: print()
+            print(f"{self.tiles[tile]} ", end='')
+            i += 1
+        print()
---
+++ erg2/game.py
@@ -0,0 +1,80 @@
+# Έστω ένα τετράγωνο 3*3 στο οποίο τοποθετείτε “καπάκια”. Έχετε στην κατοχή σας 27 “καπάκια”, 
+# 9 για κάθε μέγεθος (μικρό, μεσαίο, μεγάλο). Μια τριάδα που τερματίζει το παιχίνδι γίνεται οριζόντια, 
+# κάθετα ή διαγώνια. Η τριάδα αποτελείται από καπάκια είτε του ίδιου μεγέθους είτε από το μικρό προς το 
+# μεγαλύτερο. Επειδή έχετε καπάκια, ένα καπάκι μπορεί να μπει σε οποιοδήποτε τετράγωνο, αρκεί να είναι 
+# ελεύθερο ή να υπάρχει εκεί μικρότερο καπάκι. Γράψτε ένα πρόγραμμα το οποίο παίζει τυχαία το παιχνίδι 
+# 100 φορές και επιστρέφει το μέσο όρο των βημάτων για να λήξει το παιχνίδι.
+
+import random
+from entities import Cap, Board
+
+def spawnCaps(): 
+    caps = []
+    for i in range(0,27):
+        if i < 9: caps.append(Cap(1))
+        elif i >= 9 and i < 18: caps.append(Cap(2))
+        else: caps.append(Cap(3))
+    return caps
+
+def randomlyPlaceCaps(caps,board):
+    indexes = [i for i in range(0,27)]
+    vert = ['a','b','c']
+    hor = ['1','2','3']
+    for row in vert:
+        for col in hor:
+            key = f"{row}{col}"
+            idx = random.choice(indexes)
+            board.tiles[key] = caps[idx].placeOnBoard(board.tiles[key])
+            indexes.remove(idx)
+
+def isLegalMove(tile1, tile2):#cap = cap to be moved | tile = move position
+    if tile2 == None or tile1 > tile2: return True
+    return False
+
+def move(i1,i2,i3, board):
+    steps = 0
+    if isLegalMove(board.tiles[i1], board.tiles[i2]):
+        board.tiles[i1], board.tiles[i2] = board.tiles[i2], board.tiles[i1]
+        steps += 1
+    elif isLegalMove(board.tiles[i1], board.tiles[i3]):
+        board.tiles[i1], board.tiles[i3] = board.tiles[i3], board.tiles[i1]
+        steps += 1
+    elif isLegalMove(board.tiles[i2], board.tiles[i3]):
+        board.tiles[i2], board.tiles[i3] = board.tiles[i3], board.tiles[i2]
+        steps += 1
+    return steps
+
+def rowMove(board):
+    vert = ['a','b','c']
+    steps = 0
+    for row in vert:
+        i1 = f'{row}1'
+        i2 = f'{row}2'
+        i3 = f'{row}3'
+        steps = move(i1,i2,i3,board)
+    return steps
+
+def colMove(board):
+    hor = ['1','2','3']
+    steps = 0
+    for col in hor:
+        i1 = f'a{col}'
+        i2 = f'b{col}'
+        i3 = f'c{col}'
+        steps = move(i1,i2,i3,board)
+    return steps
+
+
+def game():
+    caps = spawnCaps()
+    board = Board()
+
+    randomlyPlaceCaps(caps, board)
+    board.show()
+    steps = 1 # 1 for initial placement of the caps
+    while not board.finish():
+        steps += rowMove(board)
+        steps += colMove(board)
+    print(f"Complete in {steps} steps")
+if __name__ == "__main__":
+    game()
